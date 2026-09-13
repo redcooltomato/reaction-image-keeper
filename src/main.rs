5bd5d2a68b20fcd88a2b22b77f03fc79ffebfa2e
redcooltomato <redcooltomato@gmail.com>
2026-09-13 22:12:05 +0300

    hell of a cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-/* #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release */
-/* #![expect(rustdoc::missing_crate_level_docs)] // it's an example */
-
 use std::{env::current_dir, eprintln, fs::{self, create_dir_all}, path::Path, vec};
 use anyhow::Result;
-use egui::{Margin, Sense, Stroke, Vec2};
+use egui::{Margin, Sense, Stroke, Ui, Vec2};
 use eframe::egui;
 use arboard::{Clipboard, ImageData};
 use image::{ImageReader, image_dimensions};
@@ -13,7 +10,6 @@ const MAGIC_FRACTION: f32 = 0.15;
 
 // boilerplate
 fn main() -> eframe::Result {
-   /*  env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`). */
     let options = eframe::NativeOptions::default();
     eframe::run_native(
         APP_NAME,
@@ -38,21 +34,34 @@ fn main() -> eframe::Result {
 // reaction image/video/meme/gif, whatever we re storing
 #[derive(Debug, PartialEq, Clone)]
 struct Entry {
-    name: String,
+    name: String, // with file extention
     file_path: String,
-    tags: Vec<String>,
+    tags: Vec<String>, // wip/unused
     width: u32,
     height: u32,
     img_data_raw: Vec<u8>,
 }
 
-// window itself
+// stuff for the app itself
 struct App {
     search_rq: String,
     selected_entries: Vec<Entry>,
     entries: Vec<Entry>, // todo add metadata storage n stuff
     started: bool,
     arboard_ctx: Clipboard,
+}
+
+// defaults
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            search_rq: "".to_string(),
+            selected_entries: vec![],
+            entries: vec![],
+            started: false,
+            arboard_ctx: Clipboard::new().unwrap(),
+        }
+    }
 }
 
 impl App {
@@ -81,7 +90,7 @@ impl App {
         Ok(())
     }
 
-    fn upd_entries_with_filter(&mut self) {
+    fn upd_filtered_entries(&mut self) {
         self.selected_entries.clear();
 
         for entry in &self.entries {
@@ -90,16 +99,143 @@ impl App {
             }
         }
     }
-}
 
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            search_rq: "".to_string(),
-            selected_entries: vec![],
-            entries: vec![],
-            started: false,
-            arboard_ctx: Clipboard::new().unwrap(),
+    fn copy_image_to_clipboard(clip: &mut Clipboard, entry: &Entry) {
+        match clip.set_image(ImageData {
+                width: entry.width as usize,
+                height: entry.height as usize,
+                bytes: std::borrow::Cow::from(entry.img_data_raw.clone()),
+            }) {
+            Ok(_) => (),
+            Err(e) => eprintln!("error occured while copying image to clipboard: {}", e),
+        }
+    }
+
+    //
+    // lots of ui moved here to help with indentation (at some point indentation depth reached 13)
+    //
+
+    fn search_bar_ui(&mut self, ui: &mut Ui) {
+        let search_input = egui::TextEdit::singleline(&mut self.search_rq)
+            .hint_text("searc by name")
+            .return_key(egui::KeyboardShortcut {
+                logical_key: egui::Key::Enter,
+                modifiers: egui::Modifiers::NONE,
+            })
+            .show(ui)
+            .response;
+            
+        if search_input.changed() {
+            self.upd_filtered_entries();
+        }
+    }
+
+    fn theme_change_ui(&mut self, ui: &mut Ui) {
+        let theme_change = ui.button(
+            if ui.ctx().theme() == egui::Theme::Light { "☼" }
+            else { "☾" }
+        );
+
+        if theme_change.clicked() {
+            if ui.ctx().theme() == egui::Theme::Light {
+                ui.ctx().set_theme(egui::Theme::Dark);
+            } else {
+                ui.ctx().set_theme(egui::Theme::Light);
+            }
+        }
+    }
+
+    fn reload_list_ui(&mut self, ui: &mut Ui) {
+        let reload_list_button = ui.button("  ⟳  ");
+
+        if reload_list_button.clicked() {
+            let resp = self.upd_storage(); // todo handle failure
+            if resp.is_err() {
+                eprintln!("{:?}", resp.err());
+            }
+            self.upd_filtered_entries();
+        }
+    }
+
+    fn add_entry_ui(&mut self, ui: &mut Ui) {
+        let add_entry_button = ui.button("  +  ");
+    
+        if add_entry_button.clicked() {
+            let selected_files = rfd::FileDialog::new().pick_files().unwrap_or(vec![]);
+
+            for file in selected_files {
+                let res = fs::copy(
+                    &file, 
+                    Path::new("./entries").join(&file.file_name().unwrap())
+                );
+                if res.is_err() {
+                    eprintln!("{:?}", res.err());
+                }
+            }
+
+            self.upd_storage();
+            self.upd_filtered_entries();
+        }
+    }
+
+    fn insert_entry_boxes_ui(&mut self, ui: &mut Ui, entry_deletion_queue: &mut Vec<Entry>) {
+        let window_rect = ui.ctx().input(|i| i.viewport().inner_rect).unwrap();
+        let screen_size = ui.ctx().input(|i| i.viewport().monitor_size)
+            .unwrap(); // todo handle none
+
+        ui.spacing_mut().item_spacing.x = 0.;
+
+        let perfect_size = screen_size * MAGIC_FRACTION;
+        let entry_box_size = egui::Vec2::from([
+            perfect_size.x 
+            + (ui.available_width() % perfect_size.x)
+                / (ui.available_width() / (perfect_size.x).max(1.)).max(1.),
+            perfect_size.y + (window_rect.max.y * 0.1)
+        ]);
+        // i think this is a good size?
+        
+        for entry in &self.selected_entries {
+            let (rect, resp) 
+                = ui.allocate_exact_size(entry_box_size, Sense::empty());
+
+            let uri = format!("file:///{}",
+                std::env::current_dir().unwrap()
+                .join(&entry.file_path)
+                .to_string_lossy().replace('\\', "/"))
+                .replace("./", "");
+
+            egui::Frame::group(ui.style())
+            .stroke(Stroke {
+                width: 1.,
+                color: if ui.ctx().theme() == egui::Theme::Dark
+                        { egui::Color32::from_rgb(0xff, 0xff, 0xff) }
+                    else
+                        { egui::Color32::from_rgb(0x00, 0x00, 0x00) }
+            })
+            .inner_margin(Margin { left: 0, right: 0, top: 0, bottom: 0})
+            .outer_margin(Margin { left: 0, right: 0, top: 5, bottom: 5})
+            .show(ui, |ui| {
+                ui.vertical(|ui| {
+                    ui.put(
+                        rect,
+                        egui::Image::from_uri(uri)
+                    );
+
+                    ui.horizontal(|ui| {
+                        let copy_button = ui.button("📋");
+
+                        if copy_button.clicked() {
+                            Self::copy_image_to_clipboard(&mut self.arboard_ctx, &entry);
+                        }
+
+                        let delete_button = ui.button("🗑");
+
+                        if delete_button.clicked() {
+                            entry_deletion_queue.push(entry.clone());
+                        }
+                    });
+                });
+            });
         }
     }
 }
@@ -108,76 +244,25 @@ impl eframe::App for App {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         /* println!("{:?}\n\n", self.selected_entries); */
 
-        let mut entry_deletion_queue: Vec<Entry> = vec![]; // storing ref creates borrowing hell
-
-        let window_rect = ui.ctx().input(|i| i.viewport().inner_rect).unwrap();
-        let screen_size = ui.ctx().input(|i| i.viewport().monitor_size)
-            .unwrap(); // todo handle none
+        let mut entry_deletion_queue: Vec<Entry> = vec![]; // storing ref creates borrow hell
 
         if !self.started {
             self.started = true;
             self.upd_storage();
-            self.upd_entries_with_filter();
+            self.upd_filtered_entries();
         }
 
         // top bar
         egui::Panel::top("search").show(ui, |ui| {
             ui.horizontal(|ui| {
-                let search_input = egui::TextEdit::singleline(&mut self.search_rq)
-                    .hint_text("searc by name")
-                    .return_key(egui::KeyboardShortcut {
-                        logical_key: egui::Key::Enter,
-                        modifiers: egui::Modifiers::NONE,
-                    })
-                    .show(ui)
-                    .response;
-                    
-                if search_input.changed() {
-                    self.upd_entries_with_filter();
-                }
+                self.search_bar_ui(ui);
 
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
-                    let theme_change = ui.button(
-                        if ui.ctx().theme() == egui::Theme::Light { "☼" }
-                        else { "☾" }
-                    );
+                    self.theme_change_ui(ui);
 
-                    if theme_change.clicked() {
-                        if ui.ctx().theme() == egui::Theme::Light {
-                            ui.ctx().set_theme(egui::Theme::Dark);
-                        } else {
-                            ui.ctx().set_theme(egui::Theme::Light);
-                        }
-                    }
+                    self.reload_list_ui(ui);
 
-                    let reload_list_button = ui.button("  ⟳  ");
-
-                    if reload_list_button.clicked() {
-                        let resp = self.upd_storage(); // todo handle failure
-                        if resp.is_err() {
-                            eprintln!("{:?}", resp.err());
-                        }
-                        self.upd_entries_with_filter();
-                    }
-
-                    let add_entry_button = ui.button("  +  ");
-    
-                    if add_entry_button.clicked() {
-                        let selected_files = rfd::FileDialog::new().pick_files().unwrap_or(vec![]);
-
-                        for file in selected_files {
-                            let res = fs::copy(
-                                &file, 
-                                Path::new("./entries").join(&file.file_name().unwrap())
-                            );
-                            if res.is_err() {
-                                eprintln!("{:?}", res.err());
-                            }
-                        }
-
-                        self.upd_storage();
-                        self.upd_entries_with_filter();
-                    }
+                    self.add_entry_ui(ui);
                 });
             });
         });
@@ -188,76 +273,7 @@ impl eframe::App for App {
             .show(ui, |ui| {
             egui::ScrollArea::vertical().show(ui, |ui| {
                 ui.horizontal_wrapped(|ui: &mut egui::Ui| {
-                    ui.spacing_mut().item_spacing.x = 0.;
-
-                    let perfect_size = screen_size * MAGIC_FRACTION;
-                    let entry_box_size = egui::Vec2::from([
-                        perfect_size.x 
-                        + (ui.available_width() % perfect_size.x)
-                            / (ui.available_width() / (perfect_size.x).max(1.)).max(1.),
-                        perfect_size.y + (window_rect.max.y * 0.1)
-                    ]);
-                    // i think this is a good size?
-
-                    /* println!("{:?}",
-                        entry_box_size); */
-                    
-                    for entry in &self.selected_entries {
-                        let (rect, resp) 
-                            = ui.allocate_exact_size(entry_box_size, Sense::empty());
-
-                        let uri = format!("file:///{}",
-                            std::env::current_dir().unwrap()
-                            .join(&entry.file_path)
-                            .to_string_lossy().replace('\\', "/"))
-                            .replace("./", "");
-
-                        /* println!("{uri}"); */
-
-                        /* println!("{} {}", ui.available_width(), entry_box_size); */
-
-                        let image_box = 
-                            egui::Frame::group(ui.style())
-                            .stroke(Stroke {
-                                width: 1.,
-                                color: if ui.ctx().theme() == egui::Theme::Dark
-                                        { egui::Color32::from_rgb(0xff, 0xff, 0xff) }
-                                    else
-                                        { egui::Color32::from_rgb(0x00, 0x00, 0x00) }
-                            })
-                            .inner_margin(Margin { left: 0, right: 0, top: 0, bottom: 0})
-                            .outer_margin(Margin { left: 0, right: 0, top: 5, bottom: 5})
-                            .show(ui, |ui| {
-                                /* ui.set_clip_rect(rect); */
-                                ui.vertical(|ui| {
-                                    ui.put(
-                                        rect,
-                                        egui::Image::from_uri(uri)
-                                    );
-
-                                    ui.horizontal(|ui| {
-                                        let copy_button = ui.button("📋");
-
-                                        if copy_button.clicked() {
-                                            match self.arboard_ctx.set_image(ImageData {
-                                                    width: entry.width as usize,
-                                                    height: entry.height as usize,
-                                                    bytes: std::borrow::Cow::from(entry.img_data_raw.clone()),
-                                                }) {
-                                                Ok(_) => (),
-                                                Err(e) => eprintln!("error occured while copying image to clipboard: {}", e),
-                                            }
-                                        }
-
-                                        let delete_button = ui.button("🗑");
-
-                                        if delete_button.clicked() {
-                                            entry_deletion_queue.push(entry.clone());
-                                        }
-                                    });
-                                });
-                            });
-                    }
+                    self.insert_entry_boxes_ui(ui, &mut entry_deletion_queue);
                 });
             });
         });
